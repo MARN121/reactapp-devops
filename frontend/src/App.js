dc395389b85b01f91a9fe399318a7b33e74fa6c4
MARN121 <muhammadasadrehman11@gmail.com>
2025-06-12 14:44:20 +0500

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import miku from './miku.png'; // make sure this is the correct path
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p className="App-text">Hello this is front end!</p>
-        <img src={miku} className="Bottom-logo" alt="miku" />
+        <p className="App-text">Project designed by Muhammad Asad ur Rehman</p>
+        <img src="/miku.png" className="Bottom-logo" alt="miku" />
       </header>
     </div>
   );
